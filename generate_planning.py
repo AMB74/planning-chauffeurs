e1f46a819c63c7051c8de60e7240209d43078c9f
AMB74 <patricia@altitude-montblanc.com>
2026-06-11 08:18:40 +0200

    Update generate_planning.py

--- generate_planning.py
+++ generate_planning.py
@@ -8,7 +8,7 @@ from datetime import datetime
 AIRTABLE_TOKEN  = os.environ["AIRTABLE_TOKEN"]
 AIRTABLE_BASE   = os.environ["AIRTABLE_BASE"]
 TABLE_NAME      = "SEMAINE 1"
-VIEW_NAME = "2. Attribution CHAUFFEURS"
+VIEW_NAME = "🚫 Extraction Github 🚫"
 
 # Liste fixe des chauffeurs
 CHAUFFEURS = [
