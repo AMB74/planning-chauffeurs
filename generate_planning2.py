31eda13ec2a9213c7090f0e065e88f6d718f3148
AMB74 <patricia@altitude-montblanc.com>
2026-08-12 17:47:16 +0200

    Update generate_planning2.py

--- generate_planning2.py
+++ generate_planning2.py
@@ -172,7 +172,7 @@ def main():
             "alerte":    get_text(f, "!!"),
             "type_transfert": get_text(f, "TYPE TRANSFERT") or "–",
             "type":      get_text(f, "TYPE") or "–",
-            "transfert": get_text(f, "TRANSFERT") or "–",
+            "transfert": get_text(f, "TRANSFERTS") or "–",
             "details":   get_text(f, "DÉTAILS") or "–",
             "heure_rdv": get_text(f, "HEURE RDV"),
             "depart":    get_text(f, "HÉBERGEMENT (from DÉPART)") or "–",
